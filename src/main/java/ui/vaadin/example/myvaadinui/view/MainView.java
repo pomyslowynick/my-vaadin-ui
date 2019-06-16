ea9a720b95636a09b1e3c583f3441fc0694bb65d
pomyslowynick <dark0mlet@gmail.com>
2019-06-16 22:16:52 +0100

    Added CRUD functionalities

--- src/main/java/ui/vaadin/example/myvaadinui/view/MainView.java
+++ src/main/java/ui/vaadin/example/myvaadinui/view/MainView.java
@@ -2,12 +2,15 @@ package ui.vaadin.example.myvaadinui.view;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.util.StringUtils;
+import ui.vaadin.example.myvaadinui.controllers.CustomerEditor;
 import ui.vaadin.example.myvaadinui.models.Customer;
 import ui.vaadin.example.myvaadinui.repository.CustomerRepository.CustomerRepository;
 
@@ -15,21 +18,58 @@ import ui.vaadin.example.myvaadinui.repository.CustomerRepository.CustomerReposi
 public class MainView extends VerticalLayout {
 
     private final CustomerRepository repo;
+
+    private final CustomerEditor editor;
+
     final Grid<Customer> grid;
 
-    public MainView(CustomerRepository repo) {
+    final TextField filter;
+
+    private final Button addNewBtn;
+
+    public MainView(CustomerRepository repo, CustomerEditor editor) {
         this.repo = repo;
+        this.editor = editor;
         this.grid = new Grid<>(Customer.class);
-        add(grid);
-        listCustomers("");
-        TextField filter = new TextField();
+        this.filter = new TextField();
+        this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
+
+        // build layout
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        add(actions, grid, editor);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "firstName", "lastName");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
         filter.setPlaceholder("Filter by last name");
+
+        // Hook logic to components
+
+        // Replace listing with filtered content when user changes filter
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(e -> listCustomers(e.getValue()));
-        add(filter, grid);
+
+        // Connect selected Customer to editor or hide if none is selected
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            editor.editCustomer(e.getValue());
+        });
+
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
+
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listCustomers(filter.getValue());
+        });
+
+        // Initialize listing
+        listCustomers(null);
     }
 
-    private void listCustomers(String filterText) {
+    // tag::listCustomers[]
+    void listCustomers(String filterText) {
         if (StringUtils.isEmpty(filterText)) {
             grid.setItems(repo.findAll());
         }
@@ -37,5 +77,6 @@ public class MainView extends VerticalLayout {
             grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
         }
     }
+    // end::listCustomers[]
 
 }
